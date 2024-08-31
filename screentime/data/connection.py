103d56ed603b24253f590ea4484ce2dafbcab6b0
T833498-SPARK <richcostello@outlook.co.nz>
2024-08-31 22:35:13 +1200

    refactor: create connection object

--- screentime/data/connection.py
+++ screentime/data/connection.py
@@ -5,11 +5,30 @@ BASE = "knowledgeC.db"
 SHM = "knowledgeC.db-shm"
 WAL = "knowledgeC.db-wal"
 
+class Connection:
+    """
+    A context manager class for handling SQLite database connections.
+    """
 
-def connect(file_name: str = BASE) -> sqlite3.Connection:
-    """
-    Connects to the database and returns a connection object
-    """
-    conn = sqlite3.connect(f"{PATH}/{file_name}")
-    return conn
+    def __init__(self, file_name: str = BASE):
+        self.file_name = file_name
+        self.conn = sqlite3.connect(f"{PATH}/{file_name}")
+
+    def __enter__(self):
+        return self.conn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is not None:
+            print(f"An error occurred: {exc_val}")
+        self.conn.close()
+
+
+
+if __name__ == "__main__":
+    with Connection() as conn:
+        cursor = conn.cursor()  
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+        table_names = [name[0] for name in cursor.fetchall()]
+        print(table_names)
+
 
